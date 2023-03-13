c94831552155ac5a37d9b5a32ef86427cc4c38da
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2023-03-13 18:16:22 +1100

    Update outputs.tf

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -3,5 +3,6 @@ output "instance_ip" {
 }
 
 output "secret_key" {
-  value = tls_private_key.terraform.private_key_pem
+  value     = tls_private_key.terraform.private_key_pem
+  sensitive = true
 }
