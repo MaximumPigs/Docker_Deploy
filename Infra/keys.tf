8bcd5c3ab64b873e8d4508ac8bd15c0474ffc7d2
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2023-03-13 16:40:56 +1100

    Remove requirement for a stored AWS secret

--- Infra/keys.tf
+++ Infra/keys.tf
@@ -6,12 +6,3 @@ resource "tls_private_key" "terraform" {
 resource "aws_key_pair" "generated_key" {
   public_key = tls_private_key.terraform.public_key_openssh
 }
-
-resource "aws_secretsmanager_secret" "generated_key" {
-  name = "generated_key"
-}
-
-resource "aws_secretsmanager_secret_version" "generated_key" {
-  secret_id     = aws_secretsmanager_secret.generated_key.id
-  secret_string = tls_private_key.terraform.private_key_pem
-}
