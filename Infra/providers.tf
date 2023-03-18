7af50bc45fd81d73260fe0b988ac3c4413ed5f0f
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2023-03-19 09:32:02 +1100

    Specify availability zone in provider block so all are consistent

--- Infra/providers.tf
+++ Infra/providers.tf
@@ -19,4 +19,5 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-2"
+  availability_zone = "ap-southeast-2a"
 }
--- Storage/providers.tf
+++ Storage/providers.tf
@@ -11,4 +11,5 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-2"
+  availability_zone = "ap-southeast-2a"
 }
