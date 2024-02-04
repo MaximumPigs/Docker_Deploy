70cac2f8cbc74556b230d72cf92c36a665d67c2e
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2024-02-04 14:48:17 +1100

    Update main.tf

--- Docker/main.tf
+++ Docker/main.tf
@@ -1,7 +1,7 @@
 module "palworld" {
   source          = "./modules/palworld"
   count           = var.game == "palworld" ? 1 : 0
-  server_name     = var.server.name
+  server_name     = var.server_name
   instance_ip     = var.instance_ip
   server_password = var.server_password
   discord_webhook = var.discord_webhook
@@ -10,7 +10,7 @@ module "palworld" {
 module "enshrouded" {
   source          = "./modules/enshrouded"
   count           = var.game == "enshrouded" ? 1 : 0
-  server_name     = var.server.name
+  server_name     = var.server_name
   instance_ip     = var.instance_ip
   server_password = var.server_password
   discord_webhook = var.discord_webhook
