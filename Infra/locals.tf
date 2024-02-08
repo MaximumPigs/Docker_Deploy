76a766be0d020bd3b002b6fb2456171a8d7b96ce
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2024-02-08 21:14:45 +1100

    AWS backup and restore
    Cloud init script and cmd to backup game files every 10 minutes and restore on boot.

--- Infra/locals.tf
+++ Infra/locals.tf
@@ -4,6 +4,8 @@ locals {
       name      = "palworld"
       vm_size   = "m6a.large"
       disk_size = "18"
+      save_dirs = "Pal/Saved/SaveGames/*" # Directories containing save files beneath the /data directory. Will backup and restore all contents recursively. Separate multiple values by space. If left blank whole /data folder will be backed up.
+      save_files = "Pal/Saved/Config/LinuxServer/GameUserSettings.ini" # Single save files beneath the /data directory. Will backup and restore these single files. Separate multiple values by space.
       ports = [
         {
           index      = "1" # This is required as a unique key to use in the for_each loop
@@ -17,6 +19,8 @@ locals {
       name      = "enshrouded"
       vm_size   = "m6a.large"
       disk_size = "44"
+      save_dirs = "" # Directories containing save files beneath the /data directory. Will backup and restore all contents recursively. Separate multiple values by space. If left blank whole /data folder will be backed up.
+      save_files = "" # Single save files beneath the /data directory. Will backup and restore these single files. Separate multiple values by space.
       ports = [
         {
           index      = "1" # This is required as a unique key to use in the for_each loop
