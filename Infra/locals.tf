b649812597c8648a801cb917e6f2df0687864569
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2024-02-08 21:47:46 +1100

    Comment out github aws actions.
    Use cloudinit to copy down files.

--- Infra/locals.tf
+++ Infra/locals.tf
@@ -1,11 +1,13 @@
 locals {
   games = {
     palworld = {
-      name       = "palworld"
-      vm_size    = "m6a.large"
-      disk_size  = "18"
-      save_dirs  = "Pal/Saved/SaveGames/"                              # Directories containing save files beneath the /data directory. Will backup and restore all contents recursively. Separate multiple values by space. If left blank whole /data folder will be backed up.
-      save_files = "Pal/Saved/Config/LinuxServer/GameUserSettings.ini" # Single save files beneath the /data directory. Will backup and restore these single files. Separate multiple values by space.
+      name      = "palworld"
+      vm_size   = "m6a.large"
+      disk_size = "18"
+      # Directories containing save files beneath the /data directory. Will backup and restore all contents recursively. Separate multiple values by space. If left blank whole /data folder will be backed up.
+      save_dirs = "Pal/Saved/SaveGames/"
+      # Single save files beneath the /data directory. Will backup and restore these single files. Separate multiple values by space.
+      save_files = "Pal/Saved/Config/LinuxServer/GameUserSettings.ini"
       ports = [
         {
           index      = "1" # This is required as a unique key to use in the for_each loop
@@ -16,11 +18,13 @@ locals {
       ]
     },
     enshrouded = {
-      name       = "enshrouded"
-      vm_size    = "m6a.large"
-      disk_size  = "44"
-      save_dirs  = "" # Directories containing save files beneath the /data directory. Will backup and restore all contents recursively. Separate multiple values by space. If left blank whole /data folder will be backed up.
-      save_files = "" # Single save files beneath the /data directory. Will backup and restore these single files. Separate multiple values by space.
+      name      = "enshrouded"
+      vm_size   = "m6a.large"
+      disk_size = "44"
+      # Directories containing save files beneath the /data directory. Will backup and restore all contents recursively. Separate multiple values by space. If left blank whole /data folder will be backed up.
+      save_dirs = ""
+      # Single save files beneath the /data directory. Will backup and restore these single files. Separate multiple values by space.
+      save_files = ""
       ports = [
         {
           index      = "1" # This is required as a unique key to use in the for_each loop
