92dabb827271ec7d67a138a792eca42b52f99413
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2024-02-08 21:18:22 +1100

    Add variables cloudinit

--- Infra/locals.tf
+++ Infra/locals.tf
@@ -1,10 +1,10 @@
 locals {
   games = {
     palworld = {
-      name      = "palworld"
-      vm_size   = "m6a.large"
-      disk_size = "18"
-      save_dirs = "Pal/Saved/SaveGames/*" # Directories containing save files beneath the /data directory. Will backup and restore all contents recursively. Separate multiple values by space. If left blank whole /data folder will be backed up.
+      name       = "palworld"
+      vm_size    = "m6a.large"
+      disk_size  = "18"
+      save_dirs  = "Pal/Saved/SaveGames/"                              # Directories containing save files beneath the /data directory. Will backup and restore all contents recursively. Separate multiple values by space. If left blank whole /data folder will be backed up.
       save_files = "Pal/Saved/Config/LinuxServer/GameUserSettings.ini" # Single save files beneath the /data directory. Will backup and restore these single files. Separate multiple values by space.
       ports = [
         {
@@ -16,10 +16,10 @@ locals {
       ]
     },
     enshrouded = {
-      name      = "enshrouded"
-      vm_size   = "m6a.large"
-      disk_size = "44"
-      save_dirs = "" # Directories containing save files beneath the /data directory. Will backup and restore all contents recursively. Separate multiple values by space. If left blank whole /data folder will be backed up.
+      name       = "enshrouded"
+      vm_size    = "m6a.large"
+      disk_size  = "44"
+      save_dirs  = "" # Directories containing save files beneath the /data directory. Will backup and restore all contents recursively. Separate multiple values by space. If left blank whole /data folder will be backed up.
       save_files = "" # Single save files beneath the /data directory. Will backup and restore these single files. Separate multiple values by space.
       ports = [
         {
--- Infra/main.tf
+++ Infra/main.tf
@@ -21,5 +21,11 @@ resource "aws_instance" "my_instance" {
     cpu_credits = "standard"
   }
 
-  user_data_base64 = base64encode(templatefile("cloudinit/userdata.tmpl", { gen_key = tls_private_key.terraform.public_key_openssh, AWS_ACCESS_KEY = var.AWS_BUCKET_ACCESS_KEY, AWS_SECRET_ACCESS_KEY = var.AWS_BUCKET_SECRET_ACCESS_KEY }))
+  user_data_base64 = base64encode(templatefile("cloudinit/userdata.tmpl", {
+    gen_key               = tls_private_key.terraform.public_key_openssh,
+    AWS_ACCESS_KEY        = var.AWS_BUCKET_ACCESS_KEY,
+    AWS_SECRET_ACCESS_KEY = var.AWS_BUCKET_SECRET_ACCESS_KEY,
+    save_dirs             = local.games[var.game].save_dirs,
+    save_files            = local.games[var.game].save_files
+  }))
 }
