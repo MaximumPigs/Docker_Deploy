62a8908ebfae171642b415d8813ac63b874f0969
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2024-02-11 11:29:24 +1100

    Update cloudwatch.tf

--- Infra/cloudwatch.tf
+++ Infra/cloudwatch.tf
@@ -1,12 +1,12 @@
 resource "aws_cloudwatch_metric_alarm" "no_players" {
-  alarm_name = "No Players ${var.environment} ${var.game}"
-  namespace = "AWS/EC2"
-  metric_name = "NetworkPacketsOut"
+  alarm_name          = "No Players ${var.environment} ${var.game}"
+  namespace           = "AWS/EC2"
+  metric_name         = "NetworkPacketsOut"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods = "2"
-  period = "10"
-  statistic = "Average"
-  threshold = "5000"
+  evaluation_periods  = "2"
+  period              = "10"
+  statistic           = "Average"
+  threshold           = "5000"
 
   alarm_actions = ["arn:aws:automate:${data.aws_region.current.name}:ec2:terminate"]
 
@@ -16,14 +16,14 @@ resource "aws_cloudwatch_metric_alarm" "no_players" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "unreachable" {
-  alarm_name = "Unreachable ${var.environment} ${var.game}"
-  namespace = "AWS/EC2"
-  metric_name = "StatusCheckFailed"
+  alarm_name          = "Unreachable ${var.environment} ${var.game}"
+  namespace           = "AWS/EC2"
+  metric_name         = "StatusCheckFailed"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods = "1"
-  period = "10"
-  statistic = "Average"
-  threshold = "2"
+  evaluation_periods  = "1"
+  period              = "10"
+  statistic           = "Average"
+  threshold           = "2"
 
   alarm_actions = [
     "arn:aws:automate:${data.aws_region.current.name}:ec2:recover"
