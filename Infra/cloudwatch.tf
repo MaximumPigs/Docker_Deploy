d9c45d51762c06ee47a3aca6269be0df15f5fc43
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2024-02-11 11:26:27 +1100

    Alarms and Actions
    Set an alarm for low outbound packets - terminate after 20 mins
    
    Set an alarm for unreachable - recover after 10 minutes

---
+++ Infra/cloudwatch.tf
@@ -0,0 +1,35 @@
+resource "aws_cloudwatch_metric_alarm" "no_players" {
+  alarm_name = "No Players ${var.environment} ${var.game}"
+  namespace = "AWS/EC2"
+  metric_name = "NetworkPacketsOut"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods = "2"
+  period = "10"
+  statistic = "Average"
+  threshold = "5000"
+
+  alarm_actions = ["arn:aws:automate:${data.data.aws_region.current}:ec2:terminate"]
+
+  dimensions = {
+    InstanceId = aws_instance.my_instance.id
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "unreachable" {
+  alarm_name = "Unreachable ${var.environment} ${var.game}"
+  namespace = "AWS/EC2"
+  metric_name = "StatusCheckFailed"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = "1"
+  period = "10"
+  statistic = "Average"
+  threshold = "2"
+
+  alarm_actions = [
+    "arn:aws:automate:${data.data.aws_region.current}:ec2:recover"
+  ]
+
+  dimensions = {
+    InstanceId = aws_instance.my_instance.id
+  }
+}
